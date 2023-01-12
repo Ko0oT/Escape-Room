6c90ea8fb3f9d74ba2f956a1e07c165dd31e9eb3
Ko0oT <serkokarjalo@gmail.com>
2023-01-12 21:36:49 +0300

    Добавит роутинг

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,57 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import Main from '../../pages/main/main';
 import Booking from '../../pages/booking/booking';
 import Contacts from '../../pages/contacts/contacts';
 import Login from '../../pages/login/login';
 import MyQuests from '../../pages/my-quests/my-quests';
 import Quest from '../../pages/quest/quest';
+import PrivateRoute from '../private-route/private-route';
+import NotFound from '../../pages/not-found/not-found';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App(): JSX.Element {
-  return <Main />;
+  const hasAccess = true;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/'
+          element={<Main />}
+        />
+        <Route
+          path='/login'
+          element={<Login />}
+        />
+        <Route
+          path='/quest/:id'
+          element={<Quest />}
+        />
+        <Route
+          path='/about'
+          element={<Contacts />}
+        />
+        <Route
+          path='/quest/:id/booking'
+          element={
+            <PrivateRoute hasAccess={hasAccess}>
+              <Booking />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path='/my-quests'
+          element={
+            <PrivateRoute hasAccess={hasAccess}>
+              <MyQuests />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path='*'
+          element={<NotFound />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
