30734bd94edde2afe0f11a79bf939a5189c1eaa3
Ko0oT <serkokarjalo@gmail.com>
2023-01-14 12:42:58 +0300

    Добавит валидацию на странице авторизации

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import { useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 import Footer from '../../components/footer/footer';
 import Header from '../../components/header/header';
+import { useForm, SubmitHandler, FieldValues } from 'react-hook-form';
 
 //Страница доступна только неавторизованным пользователям. Если пользователь уже авторизован и попытается попасть на страницу входа, его перекинет на главную страницу каталога квестов.
 //TODO
 
 function Login() {
+
+  const [agreementCheckboxChecked, setAgreementCheckboxChecked] = useState(false);
+
+  const {
+    register,
+    formState: {
+      errors, isValid
+    },
+    handleSubmit,
+    reset
+  } = useForm({
+    mode: 'all'
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    // eslint-disable-next-line no-alert
+    alert(JSON.stringify(data));
+    reset();
+  };
+
   return (
     <div className="wrapper">
       <Helmet>
@@ -33,8 +54,8 @@ function Login() {
           <div className="login__form">
             <form
               className="login-form"
-              action="https://echo.htmlacademy.ru/"
-              method="post"
+              // eslint-disable-next-line @typescript-eslint/no-misused-promises
+              onSubmit={handleSubmit(onSubmit)}
             >
               <div className="login-form__inner-wrapper">
                 <h1 className="title title--size-s login-form__title">Вход</h1>
@@ -44,29 +65,46 @@ function Login() {
                       E&nbsp;–&nbsp;mail
                     </label>
                     <input
+                      {...register('email', {
+                        required: 'Поле обязателько к заполнению',
+                        pattern: {
+                          value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                          message: 'Введите email в формате johndoe@example.ru'
+                        }
+                      })}
                       type="email"
                       id="email"
-                      name="email"
                       placeholder="Адрес электронной почты"
-                      required
                     />
+                    <div style={{height: 40}}>
+                      {errors?.email && <p>{errors?.email?.message as string}</p>}
+                    </div>
                   </div>
                   <div className="custom-input login-form__input">
                     <label className="custom-input__label" htmlFor="password">
                       Пароль
                     </label>
                     <input
+                      {...register('password', {
+                        required: 'Поле обязателько к заполнению',
+                        pattern: {
+                          value: /^(?=.*\d)(?=.*[A-Za-zА-Яа-яЁё])([^\s]){2,}$/,
+                          message: 'Пароль должен содержать как минимум одну букву и цифру без пробелов'
+                        }
+                      })}
                       type="password"
                       id="password"
-                      name="password"
                       placeholder="Пароль"
-                      required
                     />
+                    <div style={{height: 40}}>
+                      {errors?.password && <p>{errors?.password?.message as string}</p>}
+                    </div>
                   </div>
                 </div>
                 <button
                   className="btn btn--accent btn--general login-form__submit"
                   type="submit"
+                  disabled={!isValid || !agreementCheckboxChecked}
                 >
                   Войти
                 </button>
@@ -77,6 +115,8 @@ function Login() {
                   id="id-order-agreement"
                   name="user-agreement"
                   required
+                  checked={agreementCheckboxChecked}
+                  onChange={() => setAgreementCheckboxChecked(!agreementCheckboxChecked)}
                 />
                 <span className="custom-checkbox__icon">
                   <svg width={20} height={17} aria-hidden="true">
@@ -85,10 +125,7 @@ function Login() {
                 </span>
                 <span className="custom-checkbox__label">
                   Я&nbsp;согласен с&nbsp;
-                  <a
-                    className="link link--active-silver link--underlined"
-                    href="#"
-                  >
+                  <a className="link link--active-silver link--underlined" href="https://vk.com/volchkov_sergey" target="_blank" rel="noreferrer">
                     правилами обработки персональных данных
                   </a>
                   &nbsp;и пользовательским соглашением
